722596c6ae4106a0f2f92c1662c6e0c9ce86565d
Vyas Ramankulangara <vyas01890@outlook.com>
2020-02-10 13:59:40 -0600

    removed nodemailer and added sendgrid

--- routes/api/email.js
+++ routes/api/email.js
@@ -1,25 +1,9 @@
 /* eslint-disable indent */
+import sgMail from '@sendgrid/mail';
 import { Router } from 'express';
 import { check, validationResult } from 'express-validator/check';
-import nodemailer from 'nodemailer';
 
 const router = Router();
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.USER_EMAIL,
-        pass: process.env.USER_PASSWORD,
-    },
-});
-
-transporter.verify((error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email Ready');
-    }
-});
 
 router.post('/', [
     check('firstName', 'First Name is required').not().isEmpty(),
@@ -39,24 +23,19 @@ router.post('/', [
     const { body } = req.body;
     const content = `Name: ${firstName} ${lastName} \nEmail: ${email} \nMessage: ${body} `;
 
-    const mail = {
-        from: email,
-        to: 'info@cougarcs.com',
-        subject: 'New Message from Contact Form',
-        text: content,
-    };
-
-    transporter.sendMail(mail, (err, data) => {
-        if (err) {
-            res.json({
-                msg: err,
-            });
-        } else {
-            res.json({
-                msg: data,
-            });
-        }
-    });
+    try {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+            to: process.env.USER_EMAIL,
+            from: email,
+            subject: 'New Message from Contact Form',
+            text: content,
+        };
+        sgMail.send(msg);
+        res.status(200).send('Email sent.');
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 export default router;
