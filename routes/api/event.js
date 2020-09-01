cf4eec80e92aaedacb6ed8bbb08acc192e559044
Vyas Ramankulangara <vyas01890@outlook.com>
2020-09-01 18:53:30 -0500

    added payment route

--- routes/api/event.js
+++ routes/api/event.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
 import axios from 'axios';
 import { Router } from 'express';
 import _ from 'lodash';
@@ -5,44 +7,58 @@ import moment from 'moment';
 
 const router = new Router();
 const renameKey = (obj, oldKey, newKey) => {
-  if (oldKey !== newKey && !obj.date) {
-    Object.defineProperty(obj, newKey, Object.getOwnPropertyDescriptor(obj, oldKey));
-    delete obj[oldKey];
-  }
+	if (oldKey !== newKey && !obj.date) {
+		Object.defineProperty(
+			obj,
+			newKey,
+			Object.getOwnPropertyDescriptor(obj, oldKey)
+		);
+		delete obj[oldKey];
+	}
 };
 router.get('/', async (req, res) => {
-  const { data } = await axios.get(`https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR_ID}/events?key=${process.env.CALENDAR_API_KEY}`);
-  const now = moment();
-  let futureEvents = [];
-  let pastEvents = [];
+	const { data } = await axios.get(
+		`https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR_ID}/events?key=${process.env.CALENDAR_API_KEY}`
+	);
+	const now = moment();
+	let futureEvents = [];
+	let pastEvents = [];
 
-  data.items.map((item) => {
-    if (item.start === undefined) {
-      return;
-    }
-    if (item.start.date !== undefined) {
-      if (moment(item.start.date) > now) {
-        futureEvents.push(item);
-      }
-      if (moment(item.start.date) < now && moment(item.start.date) > moment().subtract(1, 'year')) { pastEvents.push(item); }
-    } else {
-      if (moment(item.start.dateTime) > now) {
-        futureEvents.push(item);
-      }
-      if (moment(item.start.dateTime) < now && moment(item.start.dateTime) > moment().subtract(1, 'year')) { pastEvents.push(item); }
-    }
-  });
+	data.items.map((item) => {
+		if (item.start === undefined) {
+			return;
+		}
+		if (item.start.date !== undefined) {
+			if (moment(item.start.date) > now) {
+				futureEvents.push(item);
+			}
+			if (
+				moment(item.start.date) < now &&
+				moment(item.start.date) > moment().subtract(1, 'year')
+			) {
+				pastEvents.push(item);
+			}
+		} else {
+			if (moment(item.start.dateTime) > now) {
+				futureEvents.push(item);
+			}
+			if (
+				moment(item.start.dateTime) < now &&
+				moment(item.start.dateTime) > moment().subtract(1, 'year')
+			) {
+				pastEvents.push(item);
+			}
+		}
+	});
 
+	futureEvents.forEach((obj) => renameKey(obj.start, 'dateTime', 'date'));
+	futureEvents.forEach((obj) => renameKey(obj.end, 'dateTime', 'date'));
+	pastEvents.forEach((obj) => renameKey(obj.start, 'dateTime', 'date'));
+	pastEvents.forEach((obj) => renameKey(obj.end, 'dateTime', 'date'));
 
-  futureEvents.forEach((obj) => renameKey(obj.start, 'dateTime', 'date'));
-  futureEvents.forEach((obj) => renameKey(obj.end, 'dateTime', 'date'));
-  pastEvents.forEach((obj) => renameKey(obj.start, 'dateTime', 'date'));
-  pastEvents.forEach((obj) => renameKey(obj.end, 'dateTime', 'date'));
-
-
-  futureEvents = _.sortBy(futureEvents, (o) => moment(o.start.date));
-  pastEvents = _.sortBy(pastEvents, (o) => moment(o.start.date)).reverse();
-  res.send({ futureEvents, pastEvents });
+	futureEvents = _.sortBy(futureEvents, (o) => moment(o.start.date));
+	pastEvents = _.sortBy(pastEvents, (o) => moment(o.start.date)).reverse();
+	res.send({ futureEvents, pastEvents, data });
 });
 
 export default router;
--- routes/api/payment.js
+++ routes/api/payment.js
@@ -1,10 +1,48 @@
-// import { Router } from 'express';
-// // import { check, validationResult } from 'express-validator/check';
-// // import { s3 } from '../../config/aws';
-// // import auth from '../../middleware/auth';
-// // import Member from '../../models/Member';
-// // import { upload } from '../../uploads/resume';
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
+import { Router } from 'express';
+import { check, validationResult } from 'express-validator/check';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import moment from 'moment';
+const router = Router();
 
-// const router = Router();
+router.post('/', async (req, res) => {
+	const {
+		email,
+		firstName,
+		lastName,
+		uhID,
+		classification,
+		paidUntil,
+		paymentMethod,
+		phoneNumber,
+	} = req.body;
 
-// export default router;
+	try {
+		const doc = new GoogleSpreadsheet(
+			'1fXguE-6AwXAihOkA39Ils28zn1ZkpClaFGUrJpNHodI'
+		);
+		await doc.useServiceAccountAuth({
+			client_email: process.env.client_email,
+			private_key: process.env.private_key,
+		});
+		await doc.loadInfo();
+		const sheet = doc.sheetsByIndex[0];
+		await sheet.addRow({
+			Timestamp: moment().format('MMMM Do YYYY, h:mm:ss a'),
+			Email: email,
+			'First Name': firstName,
+			'Last Name': lastName,
+			PeopleSoft: uhID,
+			Classification: classification,
+			'Paid Until': paidUntil,
+			'Payment Method': paymentMethod,
+			'Phone Number': phoneNumber,
+		});
+	} catch (err) {
+		return res.json(err);
+	}
+	res.send('Hello');
+});
+
+export default router;
