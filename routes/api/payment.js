5fdaa774fb125878679fd365b536d024e5fa9ab9
Shubh Sheth <shubhsheth8@gmail.com>
2020-09-10 20:00:28 -0500

    Updated Payment Logic and added G Sheet Integration

--- routes/api/payment.js
+++ routes/api/payment.js
@@ -2,7 +2,7 @@
 /* eslint-disable indent */
 import { Router } from 'express';
 import { check, validationResult } from 'express-validator/check';
-// import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 import Stripe from 'stripe';
 import moment from 'moment';
 import { v4 as uuidv4 } from 'uuid';
@@ -10,93 +10,113 @@ import { v4 as uuidv4 } from 'uuid';
 const router = Router();
 const stripe = new Stripe(process.env.STRIPE_API_KEY);
 
-router.post('/', async (req, res) => {
+router.post(
+	'/',
+	[
+		check('user.firstName')
+			.not().isEmpty().trim().escape(),
 
-	const {
-		token,
-		user
-	} = req.body;
-	const idempotencyKey = uuidv4();
+		check('user.lastName')
+			.not().isEmpty().trim().escape(),
 
-	let amount = 0;
-	if (user.paidUntil == "semester") {
-		amount = 1000;
-	} else if (user.paidUntil == "year") {
-		amount = 1800;
-	} else {
-		return res.status(500).json({ response: "Bad Request!" })
-	}
+		check('user.email')
+			.not().isEmpty()
+			.isEmail().normalizeEmail(),
 
-	// try {
-	// 	const payment = await stripe.paymentIntents.create({
-	// 		amount,
-	// 		currency: 'USD',
-	// 		description: 'TEST PAYMENT',
-	// 		payment_method: token,
-	// 		confirm: true
-	// 	});
+		check('user.uhID')
+			.not().isEmpty().trim().escape()
+			.isLength({ min: 7, max: 7 }).withMessage('Bad Request!'),
 
-	return stripe.customers.create({
-		email: user.email,
-		phone: user.phone,
-		name: user.firstName + ' ' + user.lastName,
-		metadata: {
-			"UH ID": user.uhID,
-			"Paid For": user.paidUntil
+		check('user.classification')
+			.not().isEmpty().trim().escape(),
+
+		check('user.paidUntil')
+			.not().isEmpty().trim().escape()
+			.matches(/(semester|year)/).withMessage('Bad Request!'),
+		
+		check('user.phone')
+			.not().isEmpty().trim().escape()
+			.matches(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/).withMessage('Bad Request!'),
+
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(500).json({ errors: errors.array() });
 		}
-	})
-		.then(customer => {
-			stripe.paymentIntents.create({
-				amount,
-				currency: 'USD',
-				description: 'Membership Payment',
-				payment_method: token,
-				customer: customer.id,
-				confirm: true,
-				receipt_email: 'secretary@cougarcs.com',
-			}, { idempotencyKey })
+
+		// console.log(req.body);
+		const {
+			token,
+			user
+		} = req.body;
+		const idempotencyKey = uuidv4();
+
+		let amount = 0;
+		if (user.paidUntil == "semester") {
+			amount = 1000;
+		} else if (user.paidUntil == "year") {
+			amount = 1800;
+		} else {
+			return res.status(500).json({ response: "Bad Request!" })
+		}
+
+		let result
+
+		// create customer
+		stripe.customers.create({
+			email: user.email,
+			phone: user.phone,
+			name: user.firstName + ' ' + user.lastName,
+			metadata: {
+				"UH ID": user.uhID,
+				"Paid For": user.paidUntil
+			}
 		})
-		.then(result => res.status(200).json(result))
-		.catch(err => {
+			// create payment intent and charge customer
+			.then(customer => {
+				stripe.paymentIntents.create({
+					amount,
+					currency: 'USD',
+					description: 'Membership Payment',
+					payment_method: token,
+					customer: customer.id,
+					confirm: true,
+					receipt_email: 'secretary@cougarcs.com',
+				}, { idempotencyKey })
+			})
+
+
+		// GOOGLE SHEETS
+		try {
+			const doc = new GoogleSpreadsheet(
+				'1fXguE-6AwXAihOkA39Ils28zn1ZkpClaFGUrJpNHodI'
+			);
+			await doc.useServiceAccountAuth({
+				client_email: process.env.client_email,
+				private_key: process.env.private_key,
+			});
+			await doc.loadInfo();
+			const sheet = doc.sheetsByIndex[0];
+			await sheet.addRow({
+				Timestamp: moment().format('MMMM Do YYYY, h:mm:ss a'),
+				Email: user.email,
+				'First Name': user.firstName,
+				'Last Name': user.lastName,
+				PeopleSoft: user.uhID,
+				Classification: user.classification,
+				'Paid Until': user.paidUntil,
+				'Payment Method': "Stripe",
+				'Phone Number': user.phone,
+			});
+		} catch (err) {
 			console.log(err);
-			res.status(503).json(err);
-		})
+			return res.json(err);
+		}
 
-	// 	console.log(payment);
-	// 	return res.status(200).json({response: "Works!"});
+		return res.status(200).json({ 'response': "WORKED!" });
 
-	// } catch (err) {
-	// 	console.log(err);
-	// 	return res.status(503).json(err);
-	// }
-
-
-	// GOOGLE SHEETS
-	// try {
-	// 	const doc = new GoogleSpreadsheet(
-	// 		'1fXguE-6AwXAihOkA39Ils28zn1ZkpClaFGUrJpNHodI'
-	// 	);
-	// 	await doc.useServiceAccountAuth({
-	// 		client_email: process.env.client_email,
-	// 		private_key: process.env.private_key,
-	// 	});
-	// 	await doc.loadInfo();
-	// 	const sheet = doc.sheetsByIndex[0];
-	// 	await sheet.addRow({
-	// 		Timestamp: moment().format('MMMM Do YYYY, h:mm:ss a'),
-	// 		Email: email,
-	// 		'First Name': firstName,
-	// 		'Last Name': lastName,
-	// 		PeopleSoft: uhID,
-	// 		Classification: classification,
-	// 		'Paid Until': paidUntil,
-	// 		'Payment Method': paymentMethod,
-	// 		'Phone Number': phoneNumber,
-	// 	});
-	// } catch (err) {
-	// 	return res.json(err);
-	// }
-	// res.send('Hello');
-});
+	}
+);
 
 export default router;
