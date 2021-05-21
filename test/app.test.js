1c7ed62572e25b3c20b7bfd7484401f4759ec9f7
Vyas Ramankulangara <vyas01890@outlook.com>
2021-05-20 23:34:47 -0500

    remove mocha and chai, added jest

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,12 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
 import request from 'supertest';
 import app from '../src/config/app';
 
 describe('Backend Home API', () => {
 	it('GET /', async () => {
 		const res = await request(app).get('/');
-		expect(res.status).to.equal(200);
-		expect(res.body).to.be.an.instanceof(Object);
-		expect(res.body).to.have.key('welcome');
-		expect(res.body.welcome).to.be.equal('CougarCS Backend 🐯');
+		expect(res.status).toEqual(200);
+		expect(res.body).toBeInstanceOf(Object);
+		expect(res.body).toHaveProperty('welcome');
+		expect(res.body.welcome).toEqual('CougarCS Backend 🐯');
 	});
 });
--- test/email.test.js
+++ test/email.test.js
@@ -1,96 +1,106 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
 import request from 'supertest';
 import app from '../src/config/app';
 
 describe('Email Validation', () => {
-	it('First Name missing', async () => {
+	test('First Name missing', async () => {
 		const res = await request(app).post('/api/send').send({
 			firstName: '',
 			lastName: 'Test',
 			email: 'test@test.com',
 			body: 'test',
 		});
-		expect(res.status).to.equal(400);
-		expect(res.body).to.have.property('message');
-		expect(res.body.message[0].msg).to.equal('First Name is required');
+		expect(res.status).toEqual(400);
+		expect(res.body).toHaveProperty('message');
+		expect(res.body.message[0].msg).toEqual('First Name is required');
 	});
 
-	it('Last Name missing', async () => {
+	test('Last Name missing', async () => {
 		const res = await request(app).post('/api/send').send({
 			firstName: 'Test',
 			lastName: '',
 			email: 'test@test.com',
 			body: 'test',
 		});
-		expect(res.status).to.equal(400);
-		expect(res.body).to.have.property('message');
-		expect(res.body.message[0].msg).to.equal('Last Name is required');
+		expect(res.status).toEqual(400);
+		expect(res.body).toHaveProperty('message');
+		expect(res.body.message[0].msg).toEqual('Last Name is required');
 	});
 
-	it('Email missing', async () => {
+	test('Email missing', async () => {
 		const res = await request(app).post('/api/send').send({
 			firstName: 'Test',
 			lastName: 'Test',
 			email: '',
 			body: 'test',
 		});
-		expect(res.status).to.equal(400);
-		expect(res.body).to.have.property('message');
-		expect(res.body.message[0].msg).to.equal('Email is required');
+		expect(res.status).toEqual(400);
+		expect(res.body).toHaveProperty('message');
+		expect(res.body.message[0].msg).toEqual('Email is required');
 	});
 
-	it('Body missing', async () => {
+	test('Body missing', async () => {
 		const res = await request(app).post('/api/send').send({
 			firstName: 'Test',
 			lastName: 'Test',
 			email: 'test@test.com',
 			body: '',
 		});
-		expect(res.status).to.equal(400);
-		expect(res.body).to.have.property('message');
-		expect(res.body.message[0].msg).to.equal('Body is required');
+		expect(res.status).toEqual(400);
+		expect(res.body).toHaveProperty('message');
+		expect(res.body.message[0].msg).toEqual('Body is required');
 	});
 
-	it('First and Last name missing', async () => {
+	test('First and Last name missing', async () => {
 		const res = await request(app).post('/api/send').send({
 			firstName: '',
 			lastName: '',
 			email: 'test@test.com',
 			body: 'Test',
 		});
-		expect(res.status).to.equal(400);
-		expect(res.body).to.have.property('message');
-		expect(res.body.message).to.have.lengthOf(2);
-		expect(res.body.message[0].msg).to.equal('First Name is required');
-		expect(res.body.message[1].msg).to.equal('Last Name is required');
+		expect(res.status).toEqual(400);
+		expect(res.body).toHaveProperty('message');
+		expect(res.body.message).toHaveLength(2);
+		expect(res.body.message[0].msg).toEqual('First Name is required');
+		expect(res.body.message[1].msg).toEqual('Last Name is required');
 	});
 
-	it('First name and Email missing', async () => {
+	test('First name and Email missing', async () => {
 		const res = await request(app).post('/api/send').send({
 			firstName: '',
 			lastName: 'Test',
 			email: '',
 			body: 'Test',
 		});
-		expect(res.status).to.equal(400);
-		expect(res.body).to.have.property('message');
-		expect(res.body.message).to.have.lengthOf(2);
-		expect(res.body.message[0].msg).to.equal('First Name is required');
-		expect(res.body.message[1].msg).to.equal('Email is required');
+		expect(res.status).toEqual(400);
+		expect(res.body).toHaveProperty('message');
+		expect(res.body.message).toHaveLength(2);
+		expect(res.body.message[0].msg).toEqual('First Name is required');
+		expect(res.body.message[1].msg).toEqual('Email is required');
 	});
 
-	it('Last name and Email missing', async () => {
+	test('Last name and Email missing', async () => {
 		const res = await request(app).post('/api/send').send({
 			firstName: 'Test',
 			lastName: '',
 			email: '',
 			body: 'Test',
 		});
-		expect(res.status).to.equal(400);
-		expect(res.body).to.have.property('message');
-		expect(res.body.message).to.have.lengthOf(2);
-		expect(res.body.message[0].msg).to.equal('Last Name is required');
-		expect(res.body.message[1].msg).to.equal('Email is required');
+		expect(res.status).toEqual(400);
+		expect(res.body).toHaveProperty('message');
+		expect(res.body.message).toHaveLength(2);
+		expect(res.body.message[0].msg).toEqual('Last Name is required');
+		expect(res.body.message[1].msg).toEqual('Email is required');
+	});
+
+	test('Email has send successfully', async () => {
+		const res = await request(app).post('/api/send').send({
+			firstName: 'Test',
+			lastName: 'Test',
+			email: 'test@test.com',
+			body: 'Test',
+		});
+		expect(res.status).toEqual(200);
+		expect(res.body).toHaveProperty('message');
+		expect(res.body.message).toEqual('Email sent.');
 	});
 });
