52be714d03ff0626f87810a27c16d1aabd6b1f6c
Vyas Ramankulangara <vyas01890@outlook.com>
2021-06-17 23:54:13 -0500

    refactor api

--- src/api/routes/event.js
+++ src/api/routes/event.js
@@ -1,6 +1,4 @@
 import { Router } from 'express';
-import moment from 'moment';
-import _ from 'lodash';
 import APICall from '../../utils/api/calls';
 import { logger } from '../../utils/logger';
 import { CACHE_TIME } from '../../utils/config';
@@ -9,17 +7,6 @@ import { getCache, setCache } from '../../utils/cacheData';
 const router = new Router();
 const key = 'event';
 
-const renameKey = (obj, oldKey, newKey) => {
-	if (oldKey !== newKey && !obj.date) {
-		Object.defineProperty(
-			obj,
-			newKey,
-			Object.getOwnPropertyDescriptor(obj, oldKey)
-		);
-		delete obj[oldKey];
-	}
-};
-
 router.get('/', async (req, res) => {
 	const cacheContent = getCache(key);
 	if (cacheContent) {
@@ -27,26 +14,12 @@ router.get('/', async (req, res) => {
 		return res.status(200).json(cacheContent);
 	}
 	try {
-		const data = await APICall.getEvents();
-		let events = [];
-		data.items
-			.filter((obj) => obj?.start?.date || obj?.start?.dateTime)
-			.forEach((obj) => {
-				renameKey(obj.start, 'dateTime', 'date');
-				renameKey(obj.end, 'dateTime', 'date');
-				events.push({
-					start: obj.start.date,
-					end: obj.end.date,
-					title: obj.summary,
-					desc: obj?.description ? obj.description : 'TBD',
-				});
-			});
-
-		events = _.sortBy(events, (o) => moment(o.start));
-		logger.info('Events sent');
+		const { events } = await APICall.getEvents();
 
 		setCache(key, { events }, CACHE_TIME);
 		logger.info('Stored events in cache');
+
+		logger.info('Events sent');
 		return res.status(200).json({ events });
 	} catch (err) {
 		logger.error(
