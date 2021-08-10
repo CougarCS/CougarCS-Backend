2f90d6d36c78b1ae10ccbeccf3d8ac37cd40db9b
Kevin <floreskevin51398@gmail.com>
2021-08-10 07:33:29 -0500

    fixd lint issues

--- test/apiPOST.test.js
+++ test/apiPOST.test.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 import { Router } from 'express';
 // import cougarapi from 'cougarAPI'
 
 // create tests for post/get/delete interactions
 
-const router = Router();
+// const router = Router();
