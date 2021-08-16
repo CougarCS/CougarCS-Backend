3b445ca62c7c28a2af9a101ff1021d8843bb6d8e
Nykolas Farhangi <nykolasfarhangi@gmail.com>
2021-08-16 00:47:25 -0500

    eslint fix

--- test/unit/membershipDate.test.js
+++ test/unit/membershipDate.test.js
@@ -8,141 +8,183 @@ const YEAR = '2000';
 const NEXT_YEAR = '2001';
 
 describe('Date Member Unit Test', () => {
-    test('Test Jan 1: Semester', () => {
-        const date = new Date(YEAR, 0, 1);
-        const { membershipStart, membershipEnd } = getMembershipDates(SEMESTER_TERM, date);
-        expect(membershipStart.Term).toEqual(SPRING);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(SPRING);
-        expect(membershipEnd.Year).toEqual(YEAR);
-    });
+	test('Jan 1: Semester', () => {
+		const date = new Date(YEAR, 0, 1);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			SEMESTER_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(SPRING);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(SPRING);
+		expect(membershipEnd.Year).toEqual(YEAR);
+	});
 
-    test('Test Jan 1: Year', () => {
-        const date = new Date(YEAR, 0, 1);
-        const { membershipStart, membershipEnd } = getMembershipDates(YEAR_TERM, date);
-        expect(membershipStart.Term).toEqual(SPRING);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(FALL);
-        expect(membershipEnd.Year).toEqual(YEAR);
-    });
+	test('Jan 1: Year', () => {
+		const date = new Date(YEAR, 0, 1);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			YEAR_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(SPRING);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(FALL);
+		expect(membershipEnd.Year).toEqual(YEAR);
+	});
 
-    // ---
+	// ---
 
-    test('Test May 13: Semester', () => {
-        const date = new Date(YEAR, 4, 13);
-        const { membershipStart, membershipEnd } = getMembershipDates(SEMESTER_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(FALL);
-        expect(membershipEnd.Year).toEqual(YEAR);
-    });
+	test('May 13: Semester', () => {
+		const date = new Date(YEAR, 4, 13);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			SEMESTER_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(FALL);
+		expect(membershipEnd.Year).toEqual(YEAR);
+	});
 
-    test('Test May 13: Year', () => {
-        const date = new Date(YEAR, 4, 13);
-        const { membershipStart, membershipEnd } = getMembershipDates(YEAR_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(SPRING);
-        expect(membershipEnd.Year).toEqual(NEXT_YEAR);
-    });
+	test('May 13: Year', () => {
+		const date = new Date(YEAR, 4, 13);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			YEAR_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(SPRING);
+		expect(membershipEnd.Year).toEqual(NEXT_YEAR);
+	});
 
-    // ---
+	// ---
 
-    test('Test July 15: Semester', () => {
-        const date = new Date(YEAR, 6, 15);
-        const { membershipStart, membershipEnd } = getMembershipDates(SEMESTER_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(FALL);
-        expect(membershipEnd.Year).toEqual(YEAR);
-    });
+	test('July 15: Semester', () => {
+		const date = new Date(YEAR, 6, 15);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			SEMESTER_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(FALL);
+		expect(membershipEnd.Year).toEqual(YEAR);
+	});
 
-    test('Test July 15: Year', () => {
-        const date = new Date(YEAR, 6, 15);
-        const { membershipStart, membershipEnd } = getMembershipDates(YEAR_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(SPRING);
-        expect(membershipEnd.Year).toEqual(NEXT_YEAR);
-    });
+	test('July 15: Year', () => {
+		const date = new Date(YEAR, 6, 15);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			YEAR_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(SPRING);
+		expect(membershipEnd.Year).toEqual(NEXT_YEAR);
+	});
 
-    // ---
+	// ---
 
-    test('Test August 1: Semester', () => {
-        const date = new Date(YEAR, 6, 15);
-        const { membershipStart, membershipEnd } = getMembershipDates(SEMESTER_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(FALL);
-        expect(membershipEnd.Year).toEqual(YEAR);
-    });
+	test('August 1: Semester', () => {
+		const date = new Date(YEAR, 6, 15);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			SEMESTER_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(FALL);
+		expect(membershipEnd.Year).toEqual(YEAR);
+	});
 
-    test('Test August 1: Year', () => {
-        const date = new Date(YEAR, 6, 15);
-        const { membershipStart, membershipEnd } = getMembershipDates(YEAR_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(SPRING);
-        expect(membershipEnd.Year).toEqual(NEXT_YEAR);
-    });
+	test('August 1: Year', () => {
+		const date = new Date(YEAR, 6, 15);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			YEAR_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(SPRING);
+		expect(membershipEnd.Year).toEqual(NEXT_YEAR);
+	});
 
-    // ---
+	// ---
 
-    test('Test Nov 1: Semester', () => {
-        const date = new Date(YEAR, 10, 1);
-        const { membershipStart, membershipEnd } = getMembershipDates(SEMESTER_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(FALL);
-        expect(membershipEnd.Year).toEqual(YEAR);
-    });
+	test('Nov 1: Semester', () => {
+		const date = new Date(YEAR, 10, 1);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			SEMESTER_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(FALL);
+		expect(membershipEnd.Year).toEqual(YEAR);
+	});
 
-    test('Test Nov 1: Year', () => {
-        const date = new Date(YEAR, 10, 1);
-        const { membershipStart, membershipEnd } = getMembershipDates(YEAR_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(SPRING);
-        expect(membershipEnd.Year).toEqual(NEXT_YEAR);
-    });
+	test('Nov 1: Year', () => {
+		const date = new Date(YEAR, 10, 1);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			YEAR_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(SPRING);
+		expect(membershipEnd.Year).toEqual(NEXT_YEAR);
+	});
 
-    // ---
+	// ---
 
-    test('Test Dec 1: Semester', () => {
-        const date = new Date(YEAR, 11, 1);
-        const { membershipStart, membershipEnd } = getMembershipDates(SEMESTER_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(FALL);
-        expect(membershipEnd.Year).toEqual(YEAR);
-    });
+	test('Dec 1: Semester', () => {
+		const date = new Date(YEAR, 11, 1);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			SEMESTER_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(FALL);
+		expect(membershipEnd.Year).toEqual(YEAR);
+	});
 
-    test('Test Dec 1: Year', () => {
-        const date = new Date(YEAR, 11, 1);
-        const { membershipStart, membershipEnd } = getMembershipDates(YEAR_TERM, date);
-        expect(membershipStart.Term).toEqual(FALL);
-        expect(membershipStart.Year).toEqual(YEAR);
-        expect(membershipEnd.Term).toEqual(SPRING);
-        expect(membershipEnd.Year).toEqual(NEXT_YEAR);
-    });
+	test('Dec 1: Year', () => {
+		const date = new Date(YEAR, 11, 1);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			YEAR_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(FALL);
+		expect(membershipStart.Year).toEqual(YEAR);
+		expect(membershipEnd.Term).toEqual(SPRING);
+		expect(membershipEnd.Year).toEqual(NEXT_YEAR);
+	});
 
-    // ---
+	// ---
 
-    test('Test Dec 20: Semester', () => {
-        const date = new Date(YEAR, 11, 20);
-        const { membershipStart, membershipEnd } = getMembershipDates(SEMESTER_TERM, date);
-        expect(membershipStart.Term).toEqual(SPRING);
-        expect(membershipStart.Year).toEqual(NEXT_YEAR);
-        expect(membershipEnd.Term).toEqual(SPRING);
-        expect(membershipEnd.Year).toEqual(NEXT_YEAR);
-    });
+	test('Dec 20: Semester', () => {
+		const date = new Date(YEAR, 11, 20);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			SEMESTER_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(SPRING);
+		expect(membershipStart.Year).toEqual(NEXT_YEAR);
+		expect(membershipEnd.Term).toEqual(SPRING);
+		expect(membershipEnd.Year).toEqual(NEXT_YEAR);
+	});
 
-    test('Test Dec 20: Year', () => {
-        const date = new Date(YEAR, 11, 20);
-        const { membershipStart, membershipEnd } = getMembershipDates(YEAR_TERM, date);
-        expect(membershipStart.Term).toEqual(SPRING);
-        expect(membershipStart.Year).toEqual(NEXT_YEAR);
-        expect(membershipEnd.Term).toEqual(FALL);
-        expect(membershipEnd.Year).toEqual(NEXT_YEAR);
-    });
+	test('Dec 20: Year', () => {
+		const date = new Date(YEAR, 11, 20);
+		const { membershipStart, membershipEnd } = getMembershipDates(
+			YEAR_TERM,
+			date
+		);
+		expect(membershipStart.Term).toEqual(SPRING);
+		expect(membershipStart.Year).toEqual(NEXT_YEAR);
+		expect(membershipEnd.Term).toEqual(FALL);
+		expect(membershipEnd.Year).toEqual(NEXT_YEAR);
+	});
 });
