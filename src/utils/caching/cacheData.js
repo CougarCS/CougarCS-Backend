4cdc5b32261b9aeff2804a9f70721059d6498897
Vyas Ramankulangara <vyas01890@outlook.com>
2021-11-20 23:15:45 -0600

    update chages

--- src/utils/caching/cacheData.js
+++ src/utils/caching/cacheData.js
@@ -1,39 +1,10 @@
-import opentelemetry, { context } from '@opentelemetry/api';
 import cache from './cache';
-import { TEST } from '../config';
-import { tracer } from '../tracing/tracer';
 
-const getCache = (key, parentSpan) => {
-	if (!TEST) {
-		const ctx = opentelemetry.trace.setSpan(context.active(), parentSpan);
-		const childSpan = tracer.startSpan(
-			'getCache',
-			{
-				attributes: { 'code.function': 'getCache' },
-			},
-			ctx
-		);
-
-		childSpan.setAttribute('code.filepath', 'cacheData.js');
-		childSpan.end();
-	}
+const getCache = (key) => {
 	return cache.get(key);
 };
 
-const setCache = (key, data, cacheTime, parentSpan) => {
-	if (!TEST) {
-		const ctx = opentelemetry.trace.setSpan(context.active(), parentSpan);
-		const childSpan = tracer.startSpan(
-			'setCache',
-			{
-				attributes: { 'code.function': 'setCache' },
-			},
-			ctx
-		);
-
-		childSpan.setAttribute('code.filepath', 'cacheData.js');
-		childSpan.end();
-	}
+const setCache = (key, data, cacheTime) => {
 	cache.put(key, data, cacheTime);
 };
 
