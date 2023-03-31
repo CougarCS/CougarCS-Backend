cc0e609dd2c192c5144d033bedc69cc01e85d059
Ben Key <54153434+mellophone@users.noreply.github.com>
2023-03-31 16:57:18 -0500

    Update membership post and add start script

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Ben, Mihir, and Johnny were here ;) 3/2/2023
+
+export PORT=
+export SENDGRID_API_KEY=
+export CALENDAR_ID=
+export CALENDAR_API_KEY=
+export SHEET_API=
+export RECAPTCHA_SECRET_KEY=
+export STRIPE_API_KEY=
+export SENTRY_URL=
+export SEND_EMAIL=
+export NOTION_TOKEN=
+export NOTION_TUTOR_DB=
+export YOUTUBE_API_KEY=
+export YOUTUBE_PLAYLIST_ID=
+export COUGARCS_CLOUD_URL=
+export COUGARCS_CLOUD_ACCESS_KEY=
+export COUGARCS_CLOUD_SECRET_KEY=
+export NEW_RELIC_LICENSE_KEY=
+export NEW_RELIC_APP_NAME=
+export SUPABASE_URL=
+export SUPABASE_KEY=
+yarn
+yarn build
+yarn start
